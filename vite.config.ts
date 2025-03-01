ab40b7ba9b967708432dd66b2166bb1339c710a1
butch888 <never_man@mail.ru>
2025-03-02 01:29:06 +0300

    Changed theme_color

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import { ManifestOptions, VitePWA } from 'vite-plugin-pwa';
 
 const manifest:Partial<ManifestOptions> | false = {
-  "theme_color":"#787878",
+  "theme_color":"#000000",
   "background_color":"#ffffff",
   "icons":[
     {"purpose":"maskable","sizes":"512x512","src":"icon512_maskable.png","type":"image/png"},
